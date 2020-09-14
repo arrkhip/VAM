9bfce310beec0bb75d93accf05dae02285424cae
Arkhipov Vladislav <arkhipovvladislav@mail.ru>
2020-09-14 12:03:07 +0300

    created map

--- src/blocks/map/map.js
+++ src/blocks/map/map.js
@@ -1,13 +1,19 @@
-$(".map__dealer-item").click(function() {
-	$(this).parent().find(".map__dealer-item").removeClass("active")
+// $(".map__dealer-item").click(function() {
+$(".map__dealer-header").click(function() {
+	 // $(".map__dealer-item").not(this).parent().find(".map__dealer-item").removeClass("active")         
+	// $(".map__dealer-item").parent().find(".map__dealer-item").removeClass("active")
 	// $(this).toggleClass("active")
 
-if (!$(this).hasClass('active')) {
-    $(this).addClass('active')
-} else {
-    $(this).removeClass('active');
-}
+	if (!$(this).closest(".map__dealer-item").hasClass('active')) {
+		$(".map__dealer-item").parent().find(".map__dealer-item").removeClass("active")
+		$(this).closest(".map__dealer-item").addClass('active')
+	} else {
+		$(this).closest(".map__dealer-item").removeClass('active');
+	}
+
+});
 
 
-
+$(".js-map-close").click(function() {
+	$(this).closest(".map__dealer-item").removeClass("active")
 });
