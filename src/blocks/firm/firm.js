3b42a25007dc84344e1ace5ceddc6a79fb65dcac
Arkhipov Vladislav <arkhipovvladislav@mail.ru>
2020-09-16 16:14:31 +0300

    add content and fix bags

--- src/blocks/firm/firm.js
+++ src/blocks/firm/firm.js
@@ -1,5 +1,6 @@
 var galleryThumbs = new Swiper('.firm__slider-thumbs', {
       spaceBetween: 12,
+      loop: true,
       slidesPerView: 5,
       freeMode: true,
       watchSlidesVisibility: true,
@@ -12,6 +13,7 @@ var galleryThumbs = new Swiper('.firm__slider-thumbs', {
     });
     var galleryTop = new Swiper('.firm__slider-top', {
       spaceBetween: 10,
+      loop: true,
       navigation: {
         nextEl: '.firm__slider-next',
         prevEl: '.firm__slider-prev',
