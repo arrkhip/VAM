3a6d005dd865a4269e31f305efcd6239f3399c9e
Arkhipov Vladislav <arkhipovvladislav@mail.ru>
2020-09-30 06:15:16 +0300

    creating form-request--excursion

--- src/blocks/form-request/form-request.js
+++ src/blocks/form-request/form-request.js
@@ -40,3 +40,35 @@ $('.form-request__btn').on('click', function() {
     }, 1000);
   }
 });
+
+// form-request--excursion
+if ($("#form-excursion").length){
+  $(function() {
+    document.getElementById('form-excursion').addEventListener('submit', function(evt){
+      var http = new XMLHttpRequest(), f = this;
+      var th = $(this);
+      evt.preventDefault();
+      http.open('POST', './send-excursion.php', true);
+      http.onreadystatechange = function() {
+        if (http.readyState == 4 && http.status == 200) {
+          th.find('input').val('');
+          th.trigger("reset");
+          $('.upload-file__message').removeClass('active');
+          $.fancybox.close();
+          $.fancybox.open({
+            src  : '#modal-thanks',
+            type : 'inline'
+          });
+        if (http.responseText.indexOf(f.name.value) == 0) { // очистить поля формы, если в ответе первым словом будет имя отправителя (nameFF)
+          th.trigger('reset');
+        }
+      }
+    }
+    http.onerror = function() {
+      alert('Ошибка, попробуйте еще раз');
+    }
+    http.send(new FormData(f));
+  }, false);
+
+  });
+}
